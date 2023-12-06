50f6e3143e41bfb341d8e190da0c4d18ef95a5a6
al-imam <alimam01828@gmail.com>
2023-12-06 18:18:18 +0600

    fix lint issue

--- app/theme/page.tsx
+++ app/theme/page.tsx
@@ -1,8 +1,8 @@
-import { notFound } from "next/navigation";
-import { ThemePreview } from "/components/theme-preview";
+import { notFound } from 'next/navigation'
+import { ThemePreview } from '/components/theme-preview'
 
 export default function ThemePreviewPage() {
-  if (process.env.NODE_ENV !== "development") notFound();
+  if (process.env.NODE_ENV !== 'development') notFound()
 
-  return <ThemePreview />;
+  return <ThemePreview />
 }
--- components/theme-switcher.tsx
+++ components/theme-switcher.tsx
@@ -9,7 +9,6 @@ import {
 } from '$shadcn/ui/dropdown-menu'
 import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
-import * as React from 'react'
 
 export function ThemeSwitcher() {
   const { setTheme } = useTheme()
