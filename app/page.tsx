d236891906f398ad6c36b56e9bda771621688aca
al-imam <alimam01828@gmail.com>
2023-12-11 14:51:42 +0600

    create nav-bar

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { NavBar } from '/components/nav-bar'
+
 export default function Home() {
   return (
-    <div className="content relative isolate min-h-screen overflow-hidden bg-background">
-      <h1>Hello</h1>
+    <div className="content relative isolate min-h-[200vh] overflow-hidden bg-background">
+      <NavBar />
+
+      <main className="py-6">
+        <h1>Hello</h1>
+      </main>
     </div>
   )
 }
