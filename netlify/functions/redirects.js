b4d8b2bd775d91d449eb337a577ddf1477b0121b
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-11 17:23:21 +0600

    M netlify/functions/redirects.js

--- netlify/functions/redirects.js
+++ netlify/functions/redirects.js
@@ -4,15 +4,6 @@ exports.handler = async (event, context) => {
   console.log(event)
   console.log(context)
 
-  if (!discordChannel) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        error: 'DISCORD_CHANNEL environment variable is not set',
-      }),
-    }
-  }
-
   const redirectTo = `https://cdn.discordapp.com/attachments/${process.env.DISCORD_CHANNEL}/:splat`
 
   return {
