d3883c840235c62f65126513dfca9b086d82bb4f
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-11 16:53:50 +0600

    netlify cleanup

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ import { writeFileSync } from 'fs'
 
 const source = '/media-storage-index'
 const destination = `https://cdn.discordapp.com/attachments/${process.env.DISCORD_CHANNEL}`
-writeFileSync('./_redirects', `${source}/* ${destination}/:splat 200`)
+writeFileSync(`[[redirects]]
+from = "${source}/*"
+to = "${destination}/:splat"
+status = 200`)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
