4570e1a5cd9f8108d4a9d4113934fada93246933
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-13 20:26:25 +0600

    M app/api/media/route.ts

--- app/api/media/route.ts
+++ app/api/media/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { optAuthVerifiedRouter } from '/server/next/router'
 import db from '/service/db'
 
-export const GET = optAuthVerifiedRouter(async (req, ctx, next) => {
+export const GET = optAuthVerifiedRouter(async () => {
   const media = await db.media.findMany({
     where: { status: 'APPROVED' },
     distinct: ['id'],
