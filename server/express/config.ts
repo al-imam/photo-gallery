7644a517d5dda744a06ac5b4922874d963da7712
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-13 12:12:37 +0600

    media create body update

--- server/express/config.ts
+++ server/express/config.ts
@@ -1,20 +1,13 @@
 import { MediaStatus } from '@prisma/client'
 import r from 'rype'
 
-const mediaStatus = ['PENDING', 'APPROVED', 'REJECTED', 'PASSED_TO_ADMIN'] as [
-  'PENDING',
-  'APPROVED',
-  'REJECTED',
-  'PASSED_TO_ADMIN',
-]
-mediaStatus satisfies MediaStatus[]
-
 export const mediaInputSchema = r.object({
   authorId: r.string(),
   categoryId: r.string(),
-  status_moderatedById: r.fixed(undefined as undefined | string),
   title: r.string().optional(),
   description: r.string().optional(),
   tags: r.array(r.string()).optional(),
-  status: r.string(...mediaStatus).optional(),
+
+  status: r.fixed('PENDING' as MediaStatus),
+  status_moderatedById: r.fixed(undefined as undefined | string),
 })
