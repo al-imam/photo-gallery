2fbff2e592ae9772c3d3f17055028ed5b14cc01c
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-19 05:07:03 +0600

    ping updated

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,9 +1,9 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import db from '/service/db'
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   try {
-    await db.$connect()
+    await db.$queryRawUnsafe('SELECT 1;')
     return NextResponse.json({
       db: 'connected',
       date: new Date().toString(),
