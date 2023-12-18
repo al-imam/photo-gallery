cbb26b2fa9518ad38512db421e443afba2576827
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-18 19:39:11 +0600

    ping udpated

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -4,11 +4,16 @@ import db from '/service/db'
 export async function GET(req: NextRequest) {
   try {
     await db.$connect()
-  } catch (err) {
-  } finally {
     return NextResponse.json({
-      ping: 'pong',
       ip: req.ip,
+      ping: 'pong',
+      db: 'connected',
+    })
+  } catch {
+    return NextResponse.json({
+      ip: req.ip,
+      ping: 'pong',
+      db: 'error',
     })
   }
 }
