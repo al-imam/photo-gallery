92148ccf62a4137d359b652f342dbfb4c2b802d2
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-19 14:30:34 +0600

    ping changes

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server'
 import db from '@/service/db'
 
+export const dynamic = 'force-dynamic'
 export async function GET() {
   try {
     await db.user.findFirst()
