41f5a873f9adc316e64e00c8e32b1bedc1a34008
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2023-12-24 00:35:51 +0600

    little optimazation

--- service/db.ts
+++ service/db.ts
@@ -1,14 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
 let db: PrismaClient
+const cacheKey = '___global_prisma_cache_key___'
 
 if (process.env.NODE_ENV === 'production') {
   db = new PrismaClient({ errorFormat: 'minimal' })
 } else {
-  // @ts-ignore
-  global.prisma ??= new PrismaClient({ errorFormat: 'pretty' })
-  // @ts-ignore
-  db = global.prisma
+  //@ts-ignore
+  global[cacheKey] ??= new PrismaClient({ errorFormat: 'pretty' })
+  //@ts-ignore
+  db = global[cacheKey]
 }
 
 export default db
