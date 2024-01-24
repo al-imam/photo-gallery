38717f0f01cabdd3fd7704e69009f5293e8bbd17
al-imam <alimam01828@gmail.com>
2024-01-24 17:02:25 +0600

    fix layout shift bug

---
+++ components/dashboard/fixed-popover.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { cn } from '@/shadcn/utils'
+import { useEffect, useRef, useState } from 'react'
+
+interface FixedPopoverProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {
+  children: React.ReactNode
+}
+
+export function FixedPopover({
+  children,
+  className,
+  ...rest
+}: FixedPopoverProps) {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isIntersecting, setIntersecting] = useState(true)
+
+  useEffect(() => {
+    if (!ref.current) return
+
+    const observer = new IntersectionObserver(([entry]) =>
+      setIntersecting(entry.isIntersecting)
+    )
+
+    observer.observe(ref.current)
+    return () => observer.disconnect()
+  }, [])
+
+  return (
+    <div ref={ref} className={cn('w-full')}>
+      <div
+        className={cn(
+          'ml-auto mr-[--padding] px-[--padding]',
+          {
+            'fixed inset-x-0 top-[--padding] z-50 mr-[--removed-body-scroll-bar-size]':
+              !isIntersecting,
+          },
+          className
+        )}
+        {...rest}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
