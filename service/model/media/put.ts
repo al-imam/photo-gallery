f8154f31a9833dfc4749fe799db9d563785a41da
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-01-27 00:19:55 +0600

    media update request optimized

--- service/model/media/put.ts
+++ service/model/media/put.ts
@@ -4,6 +4,7 @@ import ReqErr from '@/service/ReqError'
 import { validateAndFormatTags } from './helpers'
 import { userPermissionFactory } from '../helpers'
 import config from '@/service/config'
+import { putUpdateRequest } from './request'
 
 export type CreateMediaBody = Prettify<
   Pick<
@@ -79,28 +80,13 @@ export async function updateMedia(
       })
     }
 
-    const data = {
-      mediaId: oldMedia.id,
+    await putUpdateRequest(oldMedia.id, {
       title: body.title,
       description: body.description,
       tags: body.tags?.join(', '),
       categoryId: body.categoryId,
       hasGraphicContent: body.hasGraphicContent,
-    }
-
-    if (
-      await db.mediaUpdateRequest.findUnique({
-        where: { mediaId: oldMedia.id },
-        select: { mediaId: true },
-      })
-    ) {
-      await db.mediaUpdateRequest.update({
-        where: { mediaId: oldMedia.id },
-        data,
-      })
-    } else {
-      await db.mediaUpdateRequest.create({ data })
-    }
+    })
 
     return db.media.findUniqueOrThrow({
       where: { id: oldMedia.id },
--- service/model/media/request.ts
+++ service/model/media/request.ts
@@ -1,6 +1,6 @@
 // TODO: Implement media request model
 
-import db, { User } from '@/service/db'
+import db, { Prisma, User } from '@/service/db'
 import { PrettifyPick } from '@/service/utils'
 import { mediaPermissionFactory } from './helpers'
 import ReqErr from '@/service/ReqError'
@@ -20,6 +20,31 @@ export async function getMediaUpdateRequest(
   }
 
   return db.mediaUpdateRequest.findUnique({ where: { mediaId: requestId } })
+}
+
+export async function putUpdateRequest(
+  mediaId: string,
+  data: Omit<
+    Prisma.MediaUpdateRequestUncheckedCreateInput,
+    'mediaId' | 'modifiedAt'
+  >
+) {
+  const isExisting = await db.media.findUnique({ where: { id: mediaId } })
+
+  if (isExisting) {
+    return db.mediaUpdateRequest.update({
+      where: { mediaId: mediaId, modifiedAt: new Date() },
+      data,
+    })
+  } else {
+    return db.mediaUpdateRequest.create({
+      data: {
+        modifiedAt: new Date(),
+        mediaId,
+        ...data,
+      },
+    })
+  }
 }
 
 export async function rejectUpdateRequest() {}
