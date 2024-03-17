78a20fe80d06d7e99698f0f990e41fa87187b5d0
Nazmus Sayad <87106526+NazmusSayad@users.noreply.github.com>
2024-03-18 02:34:59 +0600

    console removed

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from 'next/server'
 import { router } from '@/server/router'
 import service from '@/service'
-import env from '@/service/env'
 
 export type PostBody = { email: string }
 export type PostData = { message: string }
